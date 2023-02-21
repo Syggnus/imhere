df29472343664b69111926799b31b73d9f932ffa
Chuan Igor <chuan.ferreira@outlook.com>
2023-02-21 20:18:42 -0300

    Add: StatusBar

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
+import { StatusBar } from "react-native";
 import { Home } from "./src/pages/home";
 
 export default function App() {
   return (
-    <Home/>
-  )
+    <>
+      <StatusBar 
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+      />
+      <Home />
+    </>
+  );
 }
